804aece675ed053525bb21b5af6fce17f373f42f
bhenderson9917 <brendanhenderson.ca@gmail.com>
2025-09-29 22:24:05 -0700

    Update section5_avg_units_per_transaction.sql

--- sql/section5_avg_units_per_transaction.sql
+++ sql/section5_avg_units_per_transaction.sql
@@ -1,9 +1,16 @@
--- Section 5: Average Product Units per Transaction
+-- Section 5: Top Wonka Customer Segmentation
 -- Table: wonka_sales
 
 SELECT
-  product_name,
-  ROUND(AVG(units),2) AS avg_units_in_transaction
+-- grabs customer ID for top performing customers
+  customer_id,
+-- counts the number of unique order IDs for the customer
+  COUNT(DISTINCT order_id) AS order_id_count,
+-- lifetime sales for the customer
+  SUM(sales) AS total_sales,
+-- divides total sales by the number of unique order IDs
+  ROUND(SUM(sales) / COUNT (DISTINCT order_id),2) AS average_sale_per_order_id
 FROM wonka_sales
-GROUP BY product_name
-ORDER BY avg_units_in_transaction DESC;
+GROUP BY customer_id
+ORDER BY total_sales DESC, order_id_count DESC, average_sale_per_order_id DESC;
+
