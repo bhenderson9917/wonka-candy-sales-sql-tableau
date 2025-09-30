7516f9bfeaffe167f4038177a692b491e885ae0f
bhenderson9917 <brendanhenderson.ca@gmail.com>
2025-09-29 22:25:11 -0700

    Update section6_product_breakdown.sql

--- sql/section6_product_breakdown.sql
+++ sql/section6_product_breakdown.sql
@@ -2,11 +2,24 @@
 -- Tables: wonka_sales (ws), product_pricing (pp)
 
 SELECT
-  ws.product_name,
-  pp.unit_price,
-  pp.unit_cost,
-  SUM(ws.units) AS total_units
-FROM wonka_sales ws
-JOIN product_pricing pp ON ws.product_id = pp.product_id
-GROUP BY ws.product_name, pp.unit_price, pp.unit_cost
-ORDER BY total_units DESC;
+  product_name,
+  unit_price,
+  unit_cost,
+-- profit margin formula utilizing unit price and unit cost
+  ROUND(((unit_price - unit_cost) / unit_price),2) AS product_profit_margin,
+-- creates profit margin buckets for products to be assigned to
+-- Note: average profitability in this case is measured manually to avoid...
+-- Note (cont.) ...windows functions & subqueries for later advanced project demonstrations
+CASE
+  WHEN ROUND(((unit_price - unit_cost) / unit_price),2) > 0.574
+    THEN 'Above Average Profitability'
+  WHEN ROUND(((unit_price - unit_cost) / unit_price),2) = 0.574
+    THEN 'Average Profitability'
+  WHEN ROUND(((unit_price - unit_cost) / unit_price),2) > 0
+    THEN 'Below Average Profitability'
+  ELSE 'Error'
+END AS profitability_bucket
+FROM product_pricing
+GROUP BY product_name, unit_price, unit_cost
+-- orders by product name
+ORDER BY product_name
